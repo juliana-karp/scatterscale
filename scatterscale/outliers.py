d56593ea400f12ea491ac01c9cb07e9819b0a816
anaviuppal <anaviuppal@gmail.com>
2026-06-25 10:18:10 -0700

    Tested functionality and fixed issues that arose with outliers and nan handling

--- scatterscale/outliers.py
+++ scatterscale/outliers.py
@@ -1,39 +1,45 @@
 import numpy as np
 
-def handle_outliers(dist, sigma_value=10, treatment='reassign'):
+def handle_outliers(data, sigma_value=10, treatment='reassign', verbose=False):
     """
     Function to identify and handle outlier values in the distribution, which would drastically change the scaling of the colorbar if not addressed.
 
     Args
-        dist (1d float array): data to be represented by the colorbar.
+        data (1d float array): data to be represented by the colorbar.
         sigma_value (int, default 10): number of stddev to use when identifying outliers, if using the sigma method.
         treatment (str, default='reassign'): how to treat the identified outliers; options are 'reassign' and 'mask_out'.
+        verbose (bool, default=False): whether to display the number of outliers identified.
     
     Returns
-        modified_dist (1d float array): data to be represented by the colorbar, treated for outliers according to the specifications.
+        data_modified (1d float array): data to be represented by the colorbar, treated for outliers according to the specifications.
     """
         
-    median = np.median(dist)
+    median = np.median(data)
     # center the gaussian at the median of the distribution
-    stddev = np.std(dist, mean=median)
+    stddev = np.std(data, mean=median)
 
     lower_bound = median - sigma_value * stddev
     upper_bound = median + sigma_value * stddev
 
-    upper_outliers =  dist > upper_bound
-    lower_outliers =  dist < lower_bound
+    upper_outliers =  data > upper_bound
+    lower_outliers =  data < lower_bound
+
+    # make a deep copy
+    data_modified = np.copy(data)
 
     # choose the method to treat outliers
 
     if treatment == 'reassign':
 
-        # make a deep copy
-        dist_modified = np.copy(dist)
-        dist_modified[upper_outliers] = upper_bound
-        dist_modified[lower_outliers] = lower_bound
+        data_modified[upper_outliers] = upper_bound
+        data_modified[lower_outliers] = lower_bound
 
     elif treatment == 'mask_out':
 
-        dist_modified = dist[~upper_outliers & ~lower_outliers]
+        data_modified[upper_outliers & lower_outliers] = np.nan
 
-    return dist_modified
+    if verbose == True:
+        print(f'{np.sum(upper_outliers)} upper outliers found.')
+        print(f'{np.sum(lower_outliers)} lower outliers found.')
+
+    return data_modified
